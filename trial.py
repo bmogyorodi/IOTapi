32a10bec7b1be7c9d9e34b90b11d2349216bf9dc
bmogyorodi <68127470+bmogyorodi@users.noreply.github.com>
2021-03-20 21:54:34 +0000

    send trial file

---
+++ trial.py
@@ -0,0 +1,17 @@
+from flask import Flask, make_response, jsonify,request
+
+webapp = Flask(__name__)
+webapp.debug=False
+def fetch_today():
+    obj={"name":"Bence","age":20}
+    return obj
+@webapp.route('/today',methods=["GET"])
+def today():
+    today=fetch_today()
+    json_day=jsonify(today)
+    resp=make_response(json_day,200)
+    return resp
+    
+if __name__ == "__main__":
+    webapp.run('0.0.0.0', port=2333) 
+
